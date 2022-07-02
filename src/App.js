53de667149eb8ac6ef94180762b9cd47c5589ddd
beyzanurakin <akinbeyzanur@gmail.com>
2022-07-02 16:39:23 +0300

    complete project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import Coin from './components/Coin'
+
 function App() {
-  return <div className='App'>hello</div>
+  const [coinsList, setCoinsList] = useState([])
+  const [query, setQuery] = useState('')
+  useEffect(() => {
+    Axios.get('https://api.coinstats.app/public/v1/coins?skip=0').then((res) =>
+      setCoinsList(res.data.coins)
+    )
+  }, [])
+  const filteredCoins = coinsList.filter((coin) => {
+    return coin.name.toLowerCase().includes(query.toLowerCase())
+  })
+  return (
+    <div className='App'>
+      <div className='cryptoHeader'>
+        <input
+          type='text'
+          placeholder='Bitcoin...'
+          onChange={(e) => {
+            setQuery(e.target.value)
+          }}
+        />
+      </div>
+      <div className='cryptoDisplay'>
+        {filteredCoins.map((coin) => {
+          return <Coin key={coin.id} {...coin} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default App
